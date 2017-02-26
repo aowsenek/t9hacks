0167be904f5b8b07eb79f9eba17f505854b3006d
Alexander Curtiss <apccurtiss@gmail.com>
2017-02-26 10:12:44 -0700

    Finished MVP

--- application.py
+++ application.py
@@ -9,7 +9,7 @@ import json
 
 c = sqlite3.connect('database.db')
 c.execute('CREATE TABLE IF NOT EXISTS users (username TEXT, password TEXT, lat TEXT, lng TEXT, gender TEXT, description TEXT)')
-c.execute('CREATE TABLE IF NOT EXISTS projects (owner TEXT, name TEXT, catagory TEXT, description TEXT, lat TEXT, lng TEXT, imglink TEXT)')
+c.execute('CREATE TABLE IF NOT EXISTS projects (owner TEXT, name TEXT, category TEXT, description TEXT, lat TEXT, lng TEXT, imglink TEXT)')
 c.close()
 
 def insert_user(username, password, location, gender, description):
@@ -39,13 +39,10 @@ def generateUserImage():
             fred = con.execute("SELECT lat, lng from users")
             todd = con.execute("SELECT lat, lng from projects")
             rows = fred.fetchall() + todd.fetchall()
-            print 'getting API call ready'
             api = 'https://beta.mapquestapi.com/staticmap/v5/getmap?size=600,400@2x&key=snDZGmb07Jc3pnSyuKxpqhQo7l6ExlEr&locations='
             for i,row in enumerate(rows):
-                print 'Adding lat/lng: %s, %s' % (row[0], row[1])
                 api += "%s,%s%s" % (row[0], row[1], "||" if (i < len(rows) - 1) else "")
-            print 'Final API call: %s' % api
-            urllib.urlretrieve(api, 'usermap.png')
+            urllib.urlretrieve(api, 'static/images/usermap.png')
 
     except Exception as e:
         print "Error: ", e
@@ -53,15 +50,14 @@ def generateUserImage():
     finally:
         con.close()
 
-def insert_project(owner, name, catagory, description, location, imglink):
-    
+def insert_project(owner, name, category, description, location, imglink):
     try:
         with sqlite3.connect("database.db") as con:
             f = urllib.urlopen("http://www.mapquestapi.com/geocoding/v1/address?key=snDZGmb07Jc3pnSyuKxpqhQo7l6ExlEr&location=%s" % location)
             js = json.loads(f.read())
             location = js['results'][0]['locations'][0]['displayLatLng']
             print "Location:", location
-            con.execute("INSERT INTO projects (owner, description, catagory, lat, lng, imglink) VALUES (?, ?, ?, ?, ?, ?)", (owner, description, location['lat'], location['lng'], imglink, ))
+            con.execute("INSERT INTO projects (owner, name, category, description, lat, lng, imglink) VALUES (?, ?, ?, ?, ?, ?, ?)", (owner, name, category, description, location['lat'], location['lng'], imglink, ))
             con.commit()
     except Exception as e:
         print "Insert project error: ", e
@@ -84,7 +80,7 @@ def verify_user(username, password):
         con.close()
     return False
 
-UPLOAD_FOLDER = 'images'
+UPLOAD_FOLDER = 'static/images'
 ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg', 'gif'])
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
@@ -113,11 +109,46 @@ def signup():
         print "Error:", e
 
 
+def get_thumbs():
+    try:
+        with sqlite3.connect("database.db") as con:
+            fred = con.execute("SELECT username, gender, description from users").fetchall()
+            todd = con.execute("SELECT owner, name, category, description, imglink from projects").fetchall()
+            thumbs='''
+            <article class="thumb">
+                <a href="static/images/usermap.png" class="image"><img src="static/images/usermap.png" alt="" /></a>
+                <h2>Users near you</h2>
+                <p>There are %d users and %d projects near you!</p>
+            </article>
+            ''' % (len(fred), len(todd))
+
+            for user in fred:
+                thumbs += '''<article class="thumb">
+                    <a href="../static/multiverseimages/thumbs/01.jpg" class="image"><img src="../static/multiverseimages/thumbs/01.jpg" alt="" /></a>
+                    <h2>User: %s</h2>
+                    <p>%s <br /> %s</p>
+                </article>
+                ''' % (user[0], user[1], user[2])
+            for project in todd:
+                thumbs += '''<article class="thumb">
+                    <a href="%s" class="image"><img src="%s" alt="" /></a>
+                    <h2>%s | %s</h2>
+                    <p>%s
+                    <br />
+                    %s</p>
+                </article>
+                ''' % (project[4], project[4], project[1], project[2], project[0], project[3])
+            return thumbs
+    except Exception as e:
+        print "Error: ", e
+        con.rollback()
+    finally:
+        con.close()
+    return ""
 
 @app.route("/projects")
 def projects():
-    return render_template('multiverse.html')
-
+    return render_template('multiverse.html', thumbs=get_thumbs())
 
 @app.route("/")
 def hello():
@@ -135,11 +166,12 @@ def makeprojet():
     token = request.cookies.get('token')
     if not token:
         print "Bad token!"
-        return render_template('index.html')
+        return render_template('multiverse.html', thumbs=get_thumbs())
     try:
         file = request.files['image']
         if file.filename == '':
             flash('No selected file')
+            print "Redirecting"
             return redirect(request.url)
         if file and allowed_file(file.filename):
             filename = secure_filename(file.filename)
@@ -147,8 +179,8 @@ def makeprojet():
             file.save(imglink)
     except Exception as e:
         print "File upload error:", e
-    insert_project(token, request.form['name'], request.form['catagory'], request.form['description'], request.form['location'], imglink)
-    return render_template('index.html')
+    insert_project(token, request.form['name'], request.form['category'], request.form['description'], request.form['location'], imglink)
+    return render_template('multiverse.html', thumbs=get_thumbs())
 
 @app.route("/profile")
 def profile():
