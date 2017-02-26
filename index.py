5db57580beeaa6a2489261a7ca5eebc20fd8f2d7
Alexander Curtiss <apccurtiss@gmail.com>
2017-02-25 22:54:10 -0700

    Got tokens working

--- index.py
+++ index.py
@@ -8,11 +8,11 @@ import urllib
 import json
 
 c = sqlite3.connect('database.db')
-c.execute('CREATE TABLE IF NOT EXISTS users (username TEXT, password TEXT, lat TEXT, lng TEXT)')
+c.execute('CREATE TABLE IF NOT EXISTS users (username TEXT, password TEXT, lat TEXT, lng TEXT, gender TEXT, description TEXT)')
 c.execute('CREATE TABLE IF NOT EXISTS projects (owner TEXT, description TEXT, imglink TEXT)')
 c.close()
 #http://www.mapquestapi.com/geocoding/v1/address?key=snDZGmb07Jc3pnSyuKxpqhQo7l6ExlEr&location=Boulder,CO
-def insert_user(username, password, location):
+def insert_user(username, password, location, gender, description):
         try:
             with sqlite3.connect("database.db") as con:
                 fred = con.execute("SELECT username from users where username = (?)", (username, ))
@@ -26,7 +26,29 @@ def insert_user(username, password, location):
 
                 if len(rows) != 0:
                     raise Exception("User already exists")
-                con.execute("INSERT INTO users (username, password, lat, lng) VALUES (?, ?)", (username, generate_password_hash(password), location['lat'], location['lng'], ))
+                con.execute("INSERT INTO users (username, password, lat, lng, gender, description) VALUES (?, ?, ?, ?, ?, ?)", (username, generate_password_hash(password), location['lat'], location['lng'], gender, description, ))
+                con.commit()
+        except Exception as e:
+            print "Error: ", e
+            con.rollback()
+        finally:
+            con.close()
+
+def insert_project(owner, description, imglink):
+        try:
+            with sqlite3.connect("database.db") as con:
+                fred = con.execute("SELECT username from users where username = (?)", (username, ))
+                rows = fred.fetchall()
+
+                print "Fetching location"
+                f = urllib.urlopen("http://www.mapquestapi.com/geocoding/v1/address?key=snDZGmb07Jc3pnSyuKxpqhQo7l6ExlEr&location=%s" % location)
+                js = json.loads(f.read())
+                location = js['results'][0]['locations'][0]['displayLatLng']
+                print "Location:", location['lat'], location['lng']
+
+                if len(rows) != 0:
+                    raise Exception("User already exists")
+                con.execute("INSERT INTO users (username, password, lat, lng, gender, description) VALUES (?, ?)", (username, generate_password_hash(password), location['lat'], location['lng'], gender, description, ))
                 con.commit()
         except Exception as e:
             print "Error: ", e
@@ -63,17 +85,20 @@ def login():
     # if request.method == 'POST':
     valid = verify_user(request.form['username'], request.form['password'])
     print ("User logged in!" if valid else "Wrong password!")
-    resp = make_response(render_template('index.html'))
+    resp = make_response(render_template('profile.html'))
     resp.set_cookie('token', request.form['username'])
     return resp
     # return render_template('index.html')
 
 @app.route("/signupform", methods = ['POST', 'GET'])
 def signup():
-    insert_user(request.form['username'], request.form['password'], request.form['location'])
-    resp = make_response(render_template('index.html'))
-    resp.set_cookie('token', request.form['username'])
-    return resp
+    try:
+        insert_user(request.form['username'], request.form['password'], request.form['location'], request.form['gender'].lower(), request.form['profile'])
+        resp = make_response(render_template('profile.html', username = request.form['username'], gender = request.form['gender'], description = request.form['profile']))
+        resp.set_cookie('token', request.form['username'])
+        return resp
+    except Exception as e:
+        print "Error:", e
 
 @app.route("/")
 def hello():
@@ -82,12 +107,34 @@ def hello():
 @app.route("/test")
 def test():
     return render_template('test.html')
-    
+
 @app.route("/signup")
 def signin():
     return render_template('signup.html')
+
+@app.route("/createproject")
+def signin():
+    return render_template('index.html')
+
 @app.route("/profile")
 def profile():
+    token = request.cookies.get('token')
+    if not token:
+        return render_template('signup.html')
+    try:
+        with sqlite3.connect("database.db") as con:
+            fred = con.execute("SELECT username, gender, description from users where username = (?)", (token, ))
+            rows = fred.fetchall()
+            print len(rows)
+            if len(rows) == 0:
+                raise Exception("Invalid token")
+            return render_template('profile.html', username=rows[0][0], gender=rows[0][1], description=rows[0][2])
+    except Exception as e:
+        print "Error: ", e
+        con.rollback()
+    finally:
+        con.close()
+
     return render_template('profile.html')
 if __name__ == "__main__":
     app.run()
