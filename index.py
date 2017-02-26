7cb555436c8a6ed4a3035caf9a0065f0673c85c1
Alexander Curtiss <apccurtiss@gmail.com>
2017-02-25 21:50:27 -0700

    Changed default page

--- index.py
+++ index.py
@@ -1,22 +1,29 @@
-from flask import Flask, request
+from flask import Flask, request, redirect, url_for
 from flask import render_template
 from flask_bootstrap import Bootstrap
 from werkzeug.security import generate_password_hash, check_password_hash
+from werkzeug.utils import secure_filename
 import sqlite3
+
+import urllib
 
 c = sqlite3.connect('database.db')
 c.execute('CREATE TABLE IF NOT EXISTS users (username TEXT, password TEXT, lat TEXT, lon TEXT)')
 c.execute('CREATE TABLE IF NOT EXISTS projects (owner TEXT, description TEXT, imglink TEXT)')
 c.close()
-
-def insert_user(username, password):
+#http://www.mapquestapi.com/geocoding/v1/address?key=snDZGmb07Jc3pnSyuKxpqhQo7l6ExlEr&location=Boulder,CO
+def insert_user(username, password, location):
         try:
             with sqlite3.connect("database.db") as con:
                 fred = con.execute("SELECT username from users where username = (?)", (username, ))
                 rows = fred.fetchall()
+                f = urllib.urlopen("http://www.mapquestapi.com/geocoding/v1/address?key=snDZGmb07Jc3pnSyuKxpqhQo7l6ExlEr&location=%s" % location)
+                js = json.reads(f.read())
+                location = js['results'][0]['locations'][0]['displayLatLng']
+                print "Location:", location['lat'], location['lon']
                 if len(rows) != 0:
                     raise Exception("User already exists")
-                con.execute("INSERT INTO users (username, password) VALUES (?, ?)", (username, generate_password_hash(password), ))
+                con.execute("INSERT INTO users (username, password, lat, lon) VALUES (?, ?)", (username, generate_password_hash(password), location['lat'], location['lon'], ))
                 con.commit()
         except Exception as e:
             print "Error: ", e
@@ -40,8 +47,32 @@ def verify_user(username, password):
         con.close()
     return False
 
+UPLOAD_FOLDER = '/images'
+ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'])
 app = Flask(__name__)
-#Bootstrap(app)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+@app.route('/test', methods=['GET', 'POST'])
+def upload_file():
+    if request.method == 'POST':
+        file = request.files['image']
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            return redirect(url_for('uploaded_file', filename=filename))
+    return '''
+    <!doctype html>
+    <title>Upload new File</title>
+    <h1>Upload new File</h1>
+    <form method=post enctype=multipart/form-data>
+      <p><input type=file name=file>
+         <input type=submit value=Upload>
+    </form>
+    '''
 
 @app.route("/login", methods = ['POST', 'GET'])
 def login():
@@ -49,22 +80,28 @@ def login():
     valid = verify_user(request.form['username'], request.form['password'])
     print ("User logged in!" if valid else "Wrong password!")
     resp = make_response(render_template('readcookie.html'))
-    resp.set_cookie('token', request.form['password'])
+    resp.set_cookie('token', request.form['username'])
     return resp
     # return render_template('index.html')
 
 
-@app.route("/signup", methods = ['POST', 'GET'])
+@app.route("/signupform", methods = ['POST', 'GET'])
 def signup():
     insert_user(request.form['username'], request.form['password'])
-    return render_template('index.html')
+    resp = make_response(render_template('readcookie.html'))
+    resp.set_cookie('token', request.form['username'])
+    return resp
 
 @app.route("/")
 def hello():
+    return render_template('index.html')
+
+@app.route("/test")
+def test():
     return render_template('test.html')
-@app.route("/signin")
+@app.route("/signup")
 def signin():
-    return render_template('signin.html')
+    return render_template('signup.html')
 @app.route("/profile")
 def profile():
     return render_template('profile.html')
